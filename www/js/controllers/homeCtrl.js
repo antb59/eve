970a7e7a0f4e67a5d6ecebe3680a00d146797a87
Antoine <antb59@gmail.com>
2016-08-27 16:13:18 +0200

    get temp

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -1,5 +1,16 @@
-angular.module('eve.controllers').controller('homeCtrl', function($scope, $state, $stateParams, $window, $log, $rootScope, configuration) {
+angular.module('eve.controllers').controller('homeCtrl', function($scope, $state, $stateParams, $window, $log, $rootScope, configuration, HomeControlService) {
 
     $log.info('[HomeCtrl] starting');
+
+    var getTemperatureRequest = HomeControlService.getTemperature();
+    getTemperatureRequest.then(function(dataResolved) {
+        scope.temperature = dataResolved.temperature;
+    },function(rejectReason) {
+        $log.error("Unable to get temperature : " + rejectReason);
+        scope.temperature = "Error";
+    },function(notifyValue) {
+        $log.info("Attempt to get temperature");
+    });
+
 
 })
