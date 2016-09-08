ac1044f40f76d7ccf52246aea01bb3a412ed2be3
Antoine <antb59@gmail.com>
2016-09-08 20:35:49 +0200

    refresh panel

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -2,37 +2,49 @@ angular.module('eve.controllers').controller('homeCtrl', function($scope, $state
 
     $log.info('[HomeCtrl] starting');
 
-    var getTemperatureRequest = HomeControlService.getTemperature();
-    getTemperatureRequest.then(function(dataResolved) {
-        $scope.temperature = dataResolved.temperature;    
-    },function(rejectReason) {
-        $log.error("Unable to get temperature : " + rejectReason);
-        $scope.errorMsg = "Unable to get temperature : " + rejectReason;
-        $scope.temperature = 0;
-    },function(notifyValue) {
-        $log.info("Attempt to get temperature");
-    });
 
-    var getLuminanceRequest = HomeControlService.getLuminance();
-    getLuminanceRequest.then(function(dataResolved) {
-        $scope.luminance = dataResolved.luminance;    
-    },function(rejectReason) {
-        $log.error("Unable to get luminance : " + rejectReason);
-        $scope.errorMsg = "Unable to get luminance : " + rejectReason;
-        $scope.luminance = 0;
-    },function(notifyValue) {
-        $log.info("Attempt to get luminance");
-    });
+    $scope.refreshTemperature = function() {
+        var getTemperatureRequest = HomeControlService.getTemperature();
+        getTemperatureRequest.then(function(dataResolved) {
+            $scope.temperature = dataResolved.temperature;    
+        },function(rejectReason) {
+            $log.error("Unable to get temperature : " + rejectReason);
+            $scope.errorMsg = "Unable to get temperature : " + rejectReason;
+            $scope.temperature = 0;
+        },function(notifyValue) {
+            $log.info("Attempt to get temperature");
+        });    
+    }; 
 
-    var getDoorStatusRequest = HomeControlService.getDoorStatus();
-    getDoorStatusRequest.then(function(dataResolved) {
-        $scope.doorStatus = dataResolved.doorStatus;    
-    },function(rejectReason) {
-        $log.error("Unable to get doorStatus : " + rejectReason);
-        $scope.errorMsg = "Unable to get doorStatus : " + rejectReason;
-        $scope.doorStatus = "UNDEFINED";
-    },function(notifyValue) {
-        $log.info("Attempt to get doorStatus");
-    });    
+
+    $scope.refreshLuminance = function() {
+        var getLuminanceRequest = HomeControlService.getLuminance();
+        getLuminanceRequest.then(function(dataResolved) {
+            $scope.luminance = dataResolved.luminance;    
+        },function(rejectReason) {
+            $log.error("Unable to get luminance : " + rejectReason);
+            $scope.errorMsg = "Unable to get luminance : " + rejectReason;
+            $scope.luminance = 0;
+        },function(notifyValue) {
+            $log.info("Attempt to get luminance");
+        });
+    }
+
+    $scope.refreshDoorStatus = function() {
+        var getDoorStatusRequest = HomeControlService.getDoorStatus();
+        getDoorStatusRequest.then(function(dataResolved) {
+            $scope.doorStatus = dataResolved.doorStatus;    
+        },function(rejectReason) {
+            $log.error("Unable to get doorStatus : " + rejectReason);
+            $scope.errorMsg = "Unable to get doorStatus : " + rejectReason;
+            $scope.doorStatus = "UNDEFINED";
+        },function(notifyValue) {
+            $log.info("Attempt to get doorStatus");
+        });    
+    }
+
+    $scope.refreshTemperature();
+    $scope.refreshLuminance();
+    $scope.refreshDoorStatus();
 
 })
