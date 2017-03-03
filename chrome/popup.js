b6a0c5f75bc73007cbef752b24a7b22ff62e0075
[a160521] Antoine Bataille <antoine.bataille@atos.net>
2017-03-03 18:01:15 +0100

    improvements

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,1 +1,28 @@
-alert("Bonjour tout le monde");
+document.write("ABE : OK OK OK");
+var automaticFill = false;
+
+function setChildTextNode(elementId, text) {
+    document.getElementById(elementId).innerText = text;
+}
+
+// Tests the roundtrip time of sendRequest().
+function testRequest() {
+    setChildTextNode("resultsRequest", "running...");
+    automaticFill = !automaticFill;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        chrome.tabs.sendRequest(tab.id, automaticFill, function handler(response) {
+            setChildTextNode("resultsRequest", response);
+        });
+    });
+
+
+    //chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+    //});
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#testRequest').addEventListener(
+        'click', testRequest);
+});
